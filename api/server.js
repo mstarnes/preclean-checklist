62fd3ca263c1694c07c76bd14fce62ce0cd9f7d5
mstarnes <mstarnes@users.noreply.github.com>
2025-08-20 19:29:48 -0500

    fix path

--- api/server.js
+++ api/server.js
@@ -373,7 +373,7 @@ app.get("/api/pending-summaries", authMiddleware, async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "public")));
-  app.get("*", (req, res) => {
+  app.get('/*path', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
