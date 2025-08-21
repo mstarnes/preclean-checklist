b8f1ecbb06e474e8990ccd6cba04857e825de7de
mstarnes <mstarnes@users.noreply.github.com>
2025-08-20 20:59:05 -0500

    removed static serving block

--- api/server.js
+++ api/server.js
@@ -363,11 +363,4 @@ app.get("/api/pending-summaries", authMiddleware, async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "public")));
-  app.get('/*path', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
-}
-
 module.exports = app;
