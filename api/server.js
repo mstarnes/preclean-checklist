e7e64e0b71edce433d007e2140d34b215488ee93
mstarnes <mstarnes@users.noreply.github.com>
2025-08-22 13:42:11 -0500

    testing https://preclean-checklist-frontend.vercel.app

--- api/server.js
+++ api/server.js
@@ -188,7 +188,7 @@ app.get(
     }).save();
 
     // Redirect with both tokens (or send in response body; adjust for security)
-    const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3003";
+    const frontendUrl = process.env.FRONTEND_URL || "https://preclean-checklist-frontend.vercel.app" || "http://localhost:3003";
     res.redirect(
       `${frontendUrl}?accessToken=${accessToken}&refreshToken=${refreshToken}`
     );
