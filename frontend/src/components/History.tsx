2755e4b8d5d93d15dcec72bb478fb94f39c45672
mstarnes <mitch.starnes@gmail.com>
2025-12-11 13:39:11 -0600

    fix trash icon on History

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -77,8 +77,8 @@ const History: React.FC = () => {
                 <p><strong>AC:</strong> {cl.cleanACFilter === 'Done' ? '✓' : ''}</p>
                 <p><strong>Comments:</strong> {cl.damagesYesNo ? cl.damagesDescription : ''}</p>
               </div>
-              <div className="flex space-x-2" onClick={e => e.stopPropagation(), handleDelete(cl._id)}>
-                {FaTrash({ onClick: () => handleDelete(cl._id), className: "cursor-pointer h-6 w-6" })}
+              <div className="flex space-x-2" onClick={e => e.stopPropagation()}>
+                {FaTrash({ onClick: () => handleDelete(cl._id), className: "cursor-pointer inline h-6 w-6" })}
               </div>
             </div>
           </div>
