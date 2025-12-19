d49c3bc08e5dd0cc418350afcc3f79ce950337d3
mstarnes <mitch.starnes@gmail.com>
2025-12-19 13:55:42 -0600

    added slider-row class to help with thumb dragging

--- frontend/src/components/ChecklistForm.tsx
+++ frontend/src/components/ChecklistForm.tsx
@@ -288,7 +288,7 @@ const ChecklistForm: React.FC = () => {
             max={max}
             value={formData[field] as number}
             onChange={(value: number) => {
-              console.log(value + " " + valueNow);
+              console.log(value);
               //setFormData(prev => ({ ...prev, [field]: value }));
             }}
             onAfterChange={(value: number) => {
